54d4e380e1f1e50c7e3a519dbca3c1895c48eab2
Daniil Nikulin <d.nikulin@innopolis.university>
2025-05-03 02:19:15 +0300

    fix: maintainability of main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pt = st.navigation([
-    st.Page(page="pages/dashboard.py", url_path="/dashboard"), 
+    st.Page(page="pages/dashboard.py", url_path="/dashboard"),
     st.Page(page="pages/auth.py", url_path="/auth"),
     st.Page(page="pages/register.py", url_path="/register")
 ])
