9b1df8bee9ae6ca0634b0a9a0f6b1c7de69db218
Daniil Nikulin <d.nikulin@innopolis.university>
2025-05-02 23:20:43 +0300

    rewrite authorization

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-pt = st.navigation([st.Page(page="pages/home.py", url_path="/home"), st.Page(page="pages/auth.py", url_path="/auth")])
+pt = st.navigation([
+    st.Page(page="pages/dashboard.py", url_path="/dashboard"), 
+    st.Page(page="pages/auth.py", url_path="/auth"),
+    st.Page(page="pages/register.py", url_path="/register")
+])
 pt.run()
