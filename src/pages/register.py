2a1d837c3d4b5f1ceca336648dc301c5e4d8be88
Daniil Nikulin <d.nikulin@innopolis.university>
2025-05-04 15:21:14 +0300

    feat: add validation to register

--- src/pages/register.py
+++ src/pages/register.py
@@ -1,6 +1,51 @@
 import streamlit as st
 import time
 import requests
+import re
+
+def validate_register_data(username_field, password_field, email_field, full_name_field):
+    is_error = False
+
+    if not username_field[1]:
+        username_field[0][0].error("Username is required")
+        is_error = True
+
+    if not password_field[1]:
+        password_field[0][0].error("Password is required")
+        is_error = True
+    elif len(password_field[1]) < 8:
+            password_field[0][0].error("Password must be at least 8 characters")
+            is_error = True
+
+    if not email_field[1]:
+        email_field[0][0].error("Email is required")
+        is_error = True
+    else:
+        match = re.match("^\\S+@\\S+\\.\\S+$", email_field[1])
+        if not match:
+            email_field[0][0].error("Invalid email format")
+            is_error = True
+
+    if not full_name_field[1]:
+        full_name_field[0][0].error("Full name is required")
+        is_error = True
+
+    return is_error
+
+def generate_fields():
+    username_ff = st.columns(1)
+    username = username_ff[0].text_input("Username")
+
+    password_ff = st.columns(1)
+    password = password_ff[0].text_input("Password", type="password")
+
+    email_ff = st.columns(1)
+    email = email_ff[0].text_input("Email")
+
+    full_name_ff = st.columns(1)
+    full_name = full_name_ff[0].text_input("Full name")
+
+    return (username_ff, username), (password_ff, password), (email_ff, email), (full_name_ff, full_name)
 
 
 def register_page():
@@ -9,29 +54,51 @@ def register_page():
     if "registered" not in st.session_state:
         st.session_state["registered"] = False
 
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    email = st.text_input("Email")
-    full_name = st.text_input("Full name")
+    if "submitted" not in st.session_state:
+        st.session_state["submitted"] = False
+
+    
+    username_field, password_field, email_field, full_name_field  = generate_fields()
+    (_, username) = username_field
+    (_, password) = password_field
+    (_, email) = email_field
+    (_, full_name) = full_name_field
+
+
+    if st.session_state["submitted"]:
+        validate_register_data(username_field, password_field, email_field, full_name_field)
+
 
     if st.button("Register"):
-        result = requests.post(
-            url="http://localhost:8000/auth/register",
-            json={
-                "username": username,
-                "password": password,
-                "email": email,
-                "full_name": full_name
-            },
-            timeout=30
-        )
-        if result.status_code == 201:
-            st.session_state["registered"] = True
-            st.success("Registration successful! Redirecting...")
-            time.sleep(2)
-            st.rerun()
+        st.session_state["submitted"] = True
+
+        is_error = validate_register_data(username_field, password_field, email_field, full_name_field)
+
+        if is_error:
+            return
         else:
-            st.error(result.json()["detail"])
+            st.session_state["submitted"] = False
+            result = requests.post(
+                url="http://localhost:8000/auth/register",
+                json={
+                    "username": username,
+                    "password": password,
+                    "email": email,
+                    "full_name": full_name
+                },
+                timeout=30
+            )
+            if result.status_code == 201:
+                st.session_state["registered"] = True
+                st.success("Registration successful! Redirecting...")
+                time.sleep(2)
+                st.rerun()
+            else:
+                try:
+                    error = result.json()["detail"]
+                    st.error(error)
+                except:
+                    st.error("Unknown error occurred. Please try again.")
 
     if st.session_state["registered"]:
         st.session_state["registered"] = False
