72780a9d57acd6038dfc9a46a69ba417b7e2d1ae
Daniil Nikulin <d.nikulin@innopolis.university>
2025-05-04 15:28:17 +0300

    feat: add validation to auth

--- src/pages/auth.py
+++ src/pages/auth.py
@@ -5,33 +5,68 @@ import requests
 
 cookie_manager = CookieManager()
 
+def validate_auth_data(username_field, password_field):
+    is_error = False
+
+    if not username_field[1]:
+        username_field[0][0].error("Username is required")
+        is_error = True
+
+    if not password_field[1]:
+        password_field[0][0].error("Password is required")
+        is_error = True
+
+    return is_error
+
+def generate_fields():
+    username_ff = st.columns(1)
+    username = username_ff[0].text_input("Username")
+
+    password_ff = st.columns(1)
+    password = password_ff[0].text_input("Password", type="password")
+
+    return (username_ff, username), (password_ff, password)
 
 def login_page():
     st.title("Authorize", anchor=False)
 
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    if "submitted" not in st.session_state:
+        st.session_state["submitted"] = False
+
+    username_field, password_field = generate_fields()
+    (_, username) = username_field
+    (_, password) = password_field
+
+    if st.session_state["submitted"]:
+        validate_auth_data(username_field, password_field)
 
     if st.button("Submit"):
-        result = requests.post(
-            url="http://localhost:8000/auth/login",
-            json={
-                "username": username,
-                "password": password,
-            },
-            timeout=30
-        )
-        if result.status_code == 200:
-            cookie_manager.set(
-                cookie="token",
-                val=result.json()["access_token"],
-                max_age=3600
-            )
-            st.success("Login successful! Redirecting...")
-            time.sleep(3)
-            st.rerun()
+        st.session_state["submitted"] = True
+
+        is_error = validate_auth_data(username_field, password_field)
+
+        if is_error:
+            return
         else:
-            st.error(result.json()["detail"])
+            result = requests.post(
+                url="http://localhost:8000/auth/login",
+                json={
+                    "username": username,
+                    "password": password,
+                },
+                timeout=30
+            )
+            if result.status_code == 200:
+                cookie_manager.set(
+                    cookie="token",
+                    val=result.json()["access_token"],
+                    max_age=3600
+                )
+                st.success("Login successful! Redirecting...")
+                time.sleep(3)
+                st.rerun()
+            else:
+                st.error(result.json()["detail"])
 
     cookies = cookie_manager.get_all()
 
